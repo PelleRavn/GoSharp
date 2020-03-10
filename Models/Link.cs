f521c99c8dbc236c5bee66ab268ba71100c1226a
Pelle Ravn <pelle@pelleravn.dk>
2020-03-10 18:04:25 +0100

    Added a links page

--- Models/Link.cs
+++ Models/Link.cs
@@ -6,12 +6,17 @@ namespace GoSharp.Models
     public class Link
     {
         public int Id { get; set; }
+
         [Required]
         public string Code { get; set; }
+
         [Required]
         public string Url { get; set; }
+
         public long Visits { get; set; } = 0L;
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
         public DateTime? UpdatedAt { get; set; }
     }
 }
---
+++ Pages/Links.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GoSharp.DbContexts;
+using GoSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoSharp.Pages
+{
+    public class LinksModel : PageModel
+    {
+        public List<Link> Links { get; set; }
+
+        public void OnGet()
+        {
+            using (var db = new GoContext())
+            {
+                Links = db.Links.ToList();
+            }
+        }
+    }
+}
